13e89901e10364d62bffa831c641f1144dae5071
lai2-24 <lai2-24@itismeucci.com>
2024-02-28 09:36:57 +0000

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Person } from './model/person';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormsModule , CommonModule] ,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,9 +15,42 @@ export class AppComponent {
   title = 'es1Angular_Dschoutezo';
   numClick = 0;
   buttonDisabled : boolean = false;
+  editMode : boolean = false;
 
-  clickButton = () => {
-    this.numClick++;
-    this.buttonDisabled = true;
+  person : Person = {
+    name: "Joan" , surname : "Dschoutezo" , age : 19
+  }
+
+  persone : Person[] = [
+    {name : "A" , surname: "B" , age:18},
+    {name : "C" , surname: "D" , age:18},
+  ]
+
+  newPerson : Person = {
+     name : " ",
+     surname : " ",
+     age : 0
+  }
+  
+  
+
+  add = () => {
+    this.persone.push(this.newPerson);
+    this.persone.splice(0,1);
+    this.newPerson = {name : " " , surname: " " , age : 0};
+  }
+
+  delete = (i: number) => {
+    this.persone.splice(i,1) ;
+  }
+
+  edit = (i: number) => {
+    this.editMode = !this.editMode;
+    this.newPerson = this.persone[i];
+  }
+
+  completaEdit = () =>{
+    this.editMode = false;
+    this.newPerson = {name : " " , surname: " " , age : 0};
   }
 }
